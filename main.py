75daffc0e1879afaf73d77040015f70f462a4b21
Jan Sebastián Kostlán <78684413+kostalonovec@users.noreply.github.com>
2022-05-08 16:37:46 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -3,35 +3,38 @@ radio.set_group(15)
 radio.set_transmit_serial_number(True)
 odpoved = 65
 cekat = False
-if cekat == False:
+#mensi ochrana proti spamování
+def on_forever():
+    global cekat, odpoved
+    if cekat == False:
 
-    if input.button_is_pressed(Button.A):
-        odpoved = 65
-        basic.show_string("A")
-        cekat = True
-    if input.button_is_pressed(Button.B):
-        odpoved = 66
-        basic.show_string("B")
-        cekat = True
-    if input.pin_is_pressed(TouchPin.P0):
-        odpoved = 67
-        basic.show_string("C")
-        cekat = True
-    if input.pin_is_pressed(TouchPin.P1):
-        odpoved = 68
-        basic.show_string("D")
-        cekat = True
-    if input.pin_is_pressed(TouchPin.P2):
-        odpoved = 69
-        basic.show_string("E")
-        cekat = True
-else:
-    pass
+        if input.button_is_pressed(Button.A):
+            odpoved = 65
+            basic.show_string("A")
+        if input.button_is_pressed(Button.B):
+            odpoved = 66
+            basic.show_string("B")
+        if input.pin_is_pressed(TouchPin.P0):
+            odpoved = 67
+            basic.show_string("C")
+        if input.pin_is_pressed(TouchPin.P1):
+            odpoved = 68
+            basic.show_string("D")
+        if input.pin_is_pressed(TouchPin.P2):
+            odpoved = 69
+            basic.show_string("E")
+        if input.logo_is_pressed():
+            radio.send_value("answer", odpoved)
+            cekat = True
+            basic.show_icon(IconNames.TARGET)
+    else:
+        pass
+forever(on_forever)
 #existuje zde další možnost a to odpoved zkonvertovat na text. Bylo by to lépe přizpůsobené pro menší změny
 
 def on_received_value(name, value):
+    global cekat, odpoved
     if name == "prijat" and value == control.device_serial_number():
         basic.show_icon(IconNames.YES)
         cekat = False
 radio.on_received_value(on_received_value)
-
--- main.ts
+++ main.ts
@@ -3,44 +3,49 @@ radio.setGroup(15)
 radio.setTransmitSerialNumber(true)
 let odpoved = 65
 let cekat = false
-if (cekat == false) {
-    if (input.buttonIsPressed(Button.A)) {
-        odpoved = 65
-        basic.showString("A")
-        cekat = true
+// mensi ochrana proti spamování
+forever(function on_forever() {
+    
+    if (cekat == false) {
+        if (input.buttonIsPressed(Button.A)) {
+            odpoved = 65
+            basic.showString("A")
+        }
+        
+        if (input.buttonIsPressed(Button.B)) {
+            odpoved = 66
+            basic.showString("B")
+        }
+        
+        if (input.pinIsPressed(TouchPin.P0)) {
+            odpoved = 67
+            basic.showString("C")
+        }
+        
+        if (input.pinIsPressed(TouchPin.P1)) {
+            odpoved = 68
+            basic.showString("D")
+        }
+        
+        if (input.pinIsPressed(TouchPin.P2)) {
+            odpoved = 69
+            basic.showString("E")
+        }
+        
+        if (input.logoIsPressed()) {
+            radio.sendValue("answer", odpoved)
+            cekat = true
+            basic.showIcon(IconNames.Target)
+        }
+        
+    } else {
+        
     }
     
-    if (input.buttonIsPressed(Button.B)) {
-        odpoved = 66
-        basic.showString("B")
-        cekat = true
-    }
-    
-    if (input.pinIsPressed(TouchPin.P0)) {
-        odpoved = 67
-        basic.showString("C")
-        cekat = true
-    }
-    
-    if (input.pinIsPressed(TouchPin.P1)) {
-        odpoved = 68
-        basic.showString("D")
-        cekat = true
-    }
-    
-    if (input.pinIsPressed(TouchPin.P2)) {
-        odpoved = 69
-        basic.showString("E")
-        cekat = true
-    }
-    
-} else {
-    
-}
-
+})
 // existuje zde další možnost a to odpoved zkonvertovat na text. Bylo by to lépe přizpůsobené pro menší změny
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    let cekat: boolean;
+    
     if (name == "prijat" && value == control.deviceSerialNumber()) {
         basic.showIcon(IconNames.Yes)
         cekat = false
